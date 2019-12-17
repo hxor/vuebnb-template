80ae6a2c9d138e84e8e4cfb0e9a216368d6c4935
idstack <idstackdeveloper@gmail.com>
2019-12-17 17:20:55 +0700

    Start With Data Binding

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: 'IDPartement',
+        address: '123 Sesame Street, City, Country',
+        about: 'This is a description about my IDPartement.'
+    }
+});
